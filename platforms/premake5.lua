f79f9207553ed507a79df274282dfa0f98b1f8bb
Olivier Chatry <olivier.chatry@gmail.com>
2015-04-26 10:22:36 +0200

    x move folders around

--- platforms/premake5.lua
+++ platforms/premake5.lua
@@ -12,12 +12,12 @@ project "vm"
 		"../vm/**.h",
 		"../vm/shaders/*.frag",
 		"../vm/shaders/*.vert",
-		"../vm/Externals/glew/src/*.c",
-		"../vm/Externals/stb/*.h"
+		"../externals/glew/src/*.c",
+		"../externals/stb/*.h"
 	}
-	includedirs {"../vm/Externals/glfw/include",
-	"../vm/Externals/glew/include",
-	"../vm/Externals/glm"}
+	includedirs {"../externals/glfw/include",
+	"../externals/glew/include",
+	"../externals/glm"}
 
 	floatingpoint "Fast"
 	flags       { "Symbols" }
@@ -36,8 +36,8 @@ project "vm"
 		defines        {"_CRT_SECURE_NO_DEPRECATE" }
 
 	configuration "windows"
-		os.copyfile("../vm/Externals/glfw/lib/glfw3.dll", "../build/glfw3.dll")
-		libdirs "../vm/Externals/glfw/lib"
+		os.copyfile("../externals/glfw/lib/glfw3.dll", "../build/glfw3.dll")
+		libdirs "../externals/glfw/lib"
 		system "Windows"
 		links {"opengl32", "glfw3dll", "Ws2_32"}
 
@@ -48,14 +48,14 @@ project "vm"
 
 	configuration "macosx"
 		platforms 	{"Universal"}
-		libdirs 	"../vm/Externals/glfw/lib/osx"
+		libdirs 	"../externals/glfw/lib/osx"
 		links       { "glfw3", "OpenGL.framework", "CoreFoundation.framework", "CoreVideo.framework", "Cocoa.framework", "IOKit.framework", "AGL.framework"}
 
 	configuration "linux"
 		if os.is64bit() then
-			libdirs "../vm/Externals/glfw/lib/linux/x86_64"
+			libdirs "../externals/glfw/lib/linux/x86_64"
 		else
-			libdirs "../vm/Externals/glfw/lib/linux/x86"
+			libdirs "../externals/glfw/lib/linux/x86"
 		end
 		links       { "glfw3", "X11", "Xxf86vm", "Xrandr", "Xi", "GL", "pthread"}
 
